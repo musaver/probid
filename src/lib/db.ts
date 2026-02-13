cbad44b29b8cf7f317dc5c31891730493a1f0055
musaver <musaverleo@gmail.com>
2026-02-13 08:35:58 +0500

    db to DO

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER!,
   password: process.env.DB_PASS!,
   database: process.env.DB_NAME!,
-  port: 3306,
+  port: parseInt(process.env.DB_PORT || '3306'),
 });
 
 export const db = drizzle(pool, { schema, mode: "default" });
--- update_schema.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `property` ADD COLUMN `sale_id` varchar(255);
-ALTER TABLE `property` ADD COLUMN `owners` json;
-ALTER TABLE `property` ADD COLUMN `winning_bid` int;
-ALTER TABLE `property` ADD COLUMN `winning_bidder_id` varchar(255);
-ALTER TABLE `property` MODIFY COLUMN `status` ENUM('active', 'sold', 'withdrawn', 'on_list', 'sold_at_tax_sale', 'redeemed', 'voided', 'cancelled', 'deed_in_progress', 'deed_issued', 'redeemed_check_issued') DEFAULT 'active';
-ALTER TABLE `property` MODIFY COLUMN `sale_id` varchar(255) NOT NULL;
